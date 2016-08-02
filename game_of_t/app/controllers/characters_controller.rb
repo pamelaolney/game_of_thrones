733eed99d3e1c9accda46e6e5e0c39be534063ed
pamelaolney <pjolney12@gmail.com>
2016-08-02 05:04:30 -0400

    able to edit characters and houses

--- game_of_t/app/controllers/characters_controller.rb
+++ game_of_t/app/controllers/characters_controller.rb
@@ -7,4 +7,21 @@ class CharactersController < ApplicationController
     @character = Character.find(params[:id])
   end
 
+  def update
+    @character = Character.find(params[:id])
+    @character.update(character_params)
+    redirect_to @character
+  end
+
+  def edit
+    @character = Character.find(params[:id])
+
+  end
+
+  private
+
+  def character_params
+    params.require(:character).permit(:name, :image_url, :title )
+  end
+
 end
